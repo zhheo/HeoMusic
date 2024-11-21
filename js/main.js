7f432d7fa020a1490d3e955f23111e6ac69a6655
张洪Heo <zhheo@qq.com>
2024-11-21 11:01:48 +0800

    最后一行歌词也能够显示在中间

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log("\n %c HeoMusic 开源静态音乐播放器 v1.5 %c https://github.com/zhheo/HeoMusic \n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;")
+console.log("\n %c HeoMusic 开源静态音乐播放器 %c https://github.com/zhheo/HeoMusic \n", "color: #fadfa3; background: #030307; padding:5px 0;", "background: #fadfa3; padding:5px 0;")
 var local = false;
 
 if (typeof userId === 'undefined') {
